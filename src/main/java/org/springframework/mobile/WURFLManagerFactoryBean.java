3b7e0d6398330c111bdabb0331b2a722f65bb981
Craig Walls <cwalls@vmware.com>
2010-08-19 15:58:55 -0500

    Fixed problem with using ClassPathResource. Being File-based, it can't load files from within JARs.

--- src/main/java/org/springframework/mobile/WURFLManagerFactoryBean.java
+++ src/main/java/org/springframework/mobile/WURFLManagerFactoryBean.java
@@ -11,7 +11,7 @@ import net.sourceforge.wurfl.core.resource.WURFLResources;
 
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 
 public class WURFLManagerFactoryBean implements FactoryBean<WURFLManager>, InitializingBean {
 
@@ -36,10 +36,12 @@ public class WURFLManagerFactoryBean implements FactoryBean<WURFLManager>, Initi
 	}
 	
 	protected WURFLManager initWURFLManager() {
-		ClassPathResource wurflZip = new ClassPathResource("wurfl.zip", getClass());
 		WURFLResources resources = new WURFLResources();
-		resources.add(new SpringXMLResource(new ClassPathResource("web_browsers_patch.xml", getClass())));
-		DefaultWURFLModel model = new DefaultWURFLModel(new SpringXMLResource(wurflZip), resources);
+		resources.add(new SpringXMLResource(new InputStreamResource(getClass().getClassLoader().getResourceAsStream(
+		        "org/springframework/mobile/web_browsers_patch.xml"))));
+
+		DefaultWURFLModel model = new DefaultWURFLModel(new SpringXMLResource(new InputStreamResource(getClass()
+		        .getClassLoader().getResourceAsStream("org/springframework/mobile/wurfl.xml"))), resources);
 		MatcherManager matcherManager = new MatcherManager(model);
 		DefaultDeviceProvider deviceProvider = new DefaultDeviceProvider(model);
 		DefaultWURFLService service = new DefaultWURFLService(matcherManager, deviceProvider);
