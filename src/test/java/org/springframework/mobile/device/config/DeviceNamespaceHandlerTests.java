b3ea41371452fd201cee035dd40cd375dc9687cb
Keith Donald <keith.donald@springsource.com>
2010-11-11 11:30:22 -0500

    spring mobile device namespace implementation

--- src/test/java/org/springframework/mobile/device/config/DeviceNamespaceHandlerTests.java
+++ src/test/java/org/springframework/mobile/device/config/DeviceNamespaceHandlerTests.java
@@ -1,17 +1,25 @@
 package org.springframework.mobile.device.config;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mobile.device.Device;
 import org.springframework.mobile.device.DeviceResolutionService;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 public class DeviceNamespaceHandlerTests {
 	
 	@Test
-	@Ignore
 	public void wurflDeviceResolutionService() throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("org/springframework/mobile/device/config/device.xml");
-		context.getBean("root", DeviceResolutionService.class);
+		DeviceResolutionService service = context.getBean("root", DeviceResolutionService.class);
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		Device device = service.resolveDevice(request);
+		assertNotNull(device);
+		assertFalse(device.isMobileBrowser());
+		assertFalse(device.isApple());
 		context.getBean("rootAndPatches", DeviceResolutionService.class);
 	}
 	
