dee9184a87463536fd583e875811219eb5ed8b1d
Roy Clarkson <rclarkson@pivotal.io>
2017-10-20 18:23:33 -0400

    Remove “audi” user-agent prefix
    Resolves #30

--- spring-mobile-device/src/main/java/org/springframework/mobile/device/LiteDeviceResolver.java
+++ spring-mobile-device/src/main/java/org/springframework/mobile/device/LiteDeviceResolver.java
@@ -218,16 +218,16 @@ public class LiteDeviceResolver implements DeviceResolver {
 	// internal helpers
 
 	private static final String[] KNOWN_MOBILE_USER_AGENT_PREFIXES = new String[] {
-			"w3c ", "w3c-", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq",
-			"bird", "blac", "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco",
-			"eric", "hipt", "htc_", "inno", "ipaq", "ipod", "jigs", "kddi", "keji",
-			"leno", "lg-c", "lg-d", "lg-g", "lge-", "lg/u", "maui", "maxo", "midp",
-			"mits", "mmef", "mobi", "mot-", "moto", "mwbp", "nec-", "newt", "noki",
-			"palm", "pana", "pant", "phil", "play", "port", "prox", "qwap", "sage",
-			"sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-",
-			"siem", "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-",
-			"tosh", "tsm-", "upg1", "upsi", "vk-v", "voda", "wap-", "wapa", "wapi",
-			"wapp", "wapr", "webc", "winw", "winw", "xda ", "xda-" };
+			"w3c ", "w3c-", "acs-", "alav", "alca", "amoi", "avan", "benq", "bird",
+			"blac", "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric",
+			"hipt", "htc_", "inno", "ipaq", "ipod", "jigs", "kddi", "keji", "leno",
+			"lg-c", "lg-d", "lg-g", "lge-", "lg/u", "maui", "maxo", "midp", "mits",
+			"mmef", "mobi", "mot-", "moto", "mwbp", "nec-", "newt", "noki", "palm",
+			"pana", "pant", "phil", "play", "port", "prox", "qwap", "sage", "sams",
+			"sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
+			"smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh",
+			"tsm-", "upg1", "upsi", "vk-v", "voda", "wap-", "wapa", "wapi", "wapp",
+			"wapr", "webc", "winw", "winw", "xda ", "xda-" };
 
 	private static final String[] KNOWN_MOBILE_USER_AGENT_KEYWORDS = new String[] {
 			"blackberry", "webos", "ipod", "lge vx", "midp", "maemo", "mmp", "mobile",
