840dd89e3add80ac2be6143253e6626f24fe923c
Roy Clarkson <rclarkson@pivotal.io>
2018-03-26 08:25:29 -0400

    Add public constructors to LiteDevice

--- spring-mobile-device/src/main/java/org/springframework/mobile/device/LiteDevice.java
+++ spring-mobile-device/src/main/java/org/springframework/mobile/device/LiteDevice.java
@@ -26,11 +26,48 @@ package org.springframework.mobile.device;
  */
 public class LiteDevice implements Device {
 
-	public static final LiteDevice NORMAL_INSTANCE = new LiteDevice(DeviceType.NORMAL, DevicePlatform.UNKNOWN);
+	public static final LiteDevice NORMAL_INSTANCE = new LiteDevice(DeviceType.NORMAL);
 
-	public static final LiteDevice MOBILE_INSTANCE = new LiteDevice(DeviceType.MOBILE, DevicePlatform.UNKNOWN);
+	public static final LiteDevice MOBILE_INSTANCE = new LiteDevice(DeviceType.MOBILE);
 
-	public static final LiteDevice TABLET_INSTANCE = new LiteDevice(DeviceType.TABLET, DevicePlatform.UNKNOWN);
+	public static final LiteDevice TABLET_INSTANCE = new LiteDevice(DeviceType.TABLET);
+
+	private final DeviceType deviceType;
+
+	private final DevicePlatform devicePlatform;
+
+	public DevicePlatform getDevicePlatform() {
+		return this.devicePlatform;
+	}
+
+	public DeviceType getDeviceType() {
+		return this.deviceType;
+	}
+
+	/**
+	 * Creates a LiteDevice with DeviceType of NORMAL and DevicePlatform UNKNOWN
+	 */
+	public LiteDevice() {
+		this(DeviceType.NORMAL, DevicePlatform.UNKNOWN);
+	}
+
+	/**
+	 * Creates a LiteDevice with DevicePlatform UNKNOWN
+	 * @param deviceType the type of device i.e. NORMAL, MOBILE, TABLET
+	 */
+	public LiteDevice(DeviceType deviceType) {
+		this(deviceType, DevicePlatform.UNKNOWN);
+	}
+
+	/**
+	 * Creates a LiteDevice
+	 * @param deviceType the type of device i.e. NORMAL, MOBILE, TABLET
+	 * @param devicePlatform the platform of device, i.e. IOS or ANDROID
+	 */
+	public LiteDevice(DeviceType deviceType, DevicePlatform devicePlatform) {
+		this.deviceType = deviceType;
+		this.devicePlatform = devicePlatform;
+	}
 
 	public boolean isNormal() {
 		return this.deviceType == DeviceType.NORMAL;
@@ -44,14 +81,6 @@ public class LiteDevice implements Device {
 		return this.deviceType == DeviceType.TABLET;
 	}
 
-	public DevicePlatform getDevicePlatform() {
-		return this.devicePlatform;
-	}
-
-	public DeviceType getDeviceType() {
-		return this.deviceType;
-	}
-
 	public static Device from(DeviceType deviceType, DevicePlatform devicePlatform) {
 		return new LiteDevice(deviceType, devicePlatform);
 	}
@@ -63,18 +92,6 @@ public class LiteDevice implements Device {
 		builder.append("type").append("=").append(this.deviceType);
 		builder.append("]");
 		return builder.toString();
-	}
-
-	private final DeviceType deviceType;
-
-	private final DevicePlatform devicePlatform;
-
-	/**
-	 * Creates a LiteDevice with DevicePlatform.
-	 */
-	private LiteDevice(DeviceType deviceType, DevicePlatform devicePlatform) {
-		this.deviceType = deviceType;
-		this.devicePlatform = devicePlatform;
 	}
 
 }
---
+++ spring-mobile-device/src/test/java/org/springframework/mobile/device/LiteDeviceTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2010-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.mobile.device;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LiteDeviceTest {
+
+	@Test
+	public void defaultDevice() {
+		LiteDevice liteDevice = new LiteDevice();
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.NORMAL);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.UNKNOWN);
+		assertThat(liteDevice.isNormal()).isTrue();
+		assertThat(liteDevice.isMobile()).isFalse();
+		assertThat(liteDevice.isTablet()).isFalse();
+	}
+
+	@Test
+	public void normalDevice() {
+		LiteDevice liteDevice = new LiteDevice(DeviceType.NORMAL);
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.NORMAL);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.UNKNOWN);
+		assertThat(liteDevice.isNormal()).isTrue();
+		assertThat(liteDevice.isMobile()).isFalse();
+		assertThat(liteDevice.isTablet()).isFalse();
+	}
+
+	@Test
+	public void mobileDevice() {
+		LiteDevice liteDevice = new LiteDevice(DeviceType.MOBILE, DevicePlatform.IOS);
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.MOBILE);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.IOS);
+		assertThat(liteDevice.isNormal()).isFalse();
+		assertThat(liteDevice.isMobile()).isTrue();
+		assertThat(liteDevice.isTablet()).isFalse();
+	}
+
+	@Test
+	public void tabletDevice() {
+		LiteDevice liteDevice = new LiteDevice(DeviceType.TABLET, DevicePlatform.ANDROID);
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.TABLET);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.ANDROID);
+		assertThat(liteDevice.isNormal()).isFalse();
+		assertThat(liteDevice.isMobile()).isFalse();
+		assertThat(liteDevice.isTablet()).isTrue();
+	}
+
+	@Test
+	public void normalInstance() {
+		LiteDevice liteDevice = LiteDevice.NORMAL_INSTANCE;
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.NORMAL);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.UNKNOWN);
+		assertThat(liteDevice.isNormal()).isTrue();
+		assertThat(liteDevice.isMobile()).isFalse();
+		assertThat(liteDevice.isTablet()).isFalse();
+	}
+
+	@Test
+	public void mobileInstance() {
+		LiteDevice liteDevice = LiteDevice.MOBILE_INSTANCE;
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.MOBILE);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.UNKNOWN);
+		assertThat(liteDevice.isNormal()).isFalse();
+		assertThat(liteDevice.isMobile()).isTrue();
+		assertThat(liteDevice.isTablet()).isFalse();
+	}
+
+	@Test
+	public void tabletInstance() {
+		LiteDevice liteDevice = LiteDevice.TABLET_INSTANCE;
+		assertThat(liteDevice.getDeviceType()).isEqualTo(DeviceType.TABLET);
+		assertThat(liteDevice.getDevicePlatform()).isEqualTo(DevicePlatform.UNKNOWN);
+		assertThat(liteDevice.isNormal()).isFalse();
+		assertThat(liteDevice.isMobile()).isFalse();
+		assertThat(liteDevice.isTablet()).isTrue();
+	}
+
+}
